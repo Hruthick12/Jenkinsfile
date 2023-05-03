467ca22517895587ee6596c7ff25ae52dba14ba8
hruthik <hruthik@gmail.com>
2023-05-03 19:30:08 +0530

    changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir /var/lib/jenkins/practice
 cd /var/lib/jenkins/practice
 aws s3 cp s3://jenkins-test-9/java_build/sample-hello-world-0.2.0.jar .
 scp sample-hello-world-0.2.0.jar ec2-user@10.0.0.183:/home/ec2-user/tomcat/apache-tomcat-9.0.74/webapps
