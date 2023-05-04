1a0195847003e2ef1de5c6af3961cff7b51afc34
hruthik <hruthik@gmail.com>
2023-05-04 11:34:35 +0530

    commit

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,7 +1,7 @@
-def checkout_git(String repo){
+def checkout_git(String repo, String branch){
 
     echo "this is for checking out git code."
-     git credentialsId: 'github', url:"git@github.com:Hruthick12/${repo}.git",branch: "master"
+     git credentialsId: 'github', url:"git@github.com:Hruthick12/${repo}.git",branch: "${branch}"
 
 }
 return this
