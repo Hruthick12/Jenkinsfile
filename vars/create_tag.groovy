1e947ecec0495ea0bf129e05121e3930cf3e0dbd
hruthik <hruthik@gmail.com>
2023-05-04 16:49:31 +0530

    changes

--- vars/create_tag.groovy
+++ vars/create_tag.groovy
@@ -3,10 +3,10 @@ def create_tag(String tag){
    sh """
    git log -1
    cd ${WORKSPACE}
-   git tag -a ${tag} -m "this tag has been created from jenkins"
+   git tag -a ${tag} -m "this tag has been created from jenkinfile"
    git push origin ${tag}
-   
-      """
+
+       """
 
 
 }
