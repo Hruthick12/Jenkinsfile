ec260ba4821bd822aeece1db12c1ce665556306a
hruthik <hruthik@gmail.com>
2023-05-06 18:25:09 +0530

    new changes

--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -2,6 +2,8 @@ def deploy_tomcat(String tag, String env) {
 
     echo "deploying to tomcat server."
     sh '''
+       echo "WORKSPACE is ${WORKSPACE}"
+       echo "environment is $env"
        cd ${WORKSPACE}/shellscripts
        sh ./deploy_java.sh ${tag} ${env}
        '''
